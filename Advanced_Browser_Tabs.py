847e6428757d544184b642b3ecc5aec4e8e58242
omar1z <omarzammar11@gmail.com>
2023-11-10 13:56:00 +0200

    Menu for user

---
+++ Advanced_Browser_Tabs.py
@@ -0,0 +1,11 @@
+def main():
+    #Menu(user interface)
+    print("1.Open Tab")
+    print("2.Close Tab")
+    print("3.Switch Tab")
+    print("4.Display All Tabs")
+    print("5.Open Nested Tab")
+    print("6.Clear All Tabs")
+    print("7.Save Tabs")
+    print("8.Import Tabs")
+    print("9.Exit")
